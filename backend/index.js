04c25091f98a2d9e02827d88e45cb5c9c5d46598
mostafa <mostafameerzad@gmail.com>
2024-04-10 21:33:24 +0430

    Complete creating a working demo

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,18 @@ app.use(cors())
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT"],
   },
 });
 
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+  socket.on("send_message", (data) => {
+    console.log(data);
+  });
+});
 
-server.listen(3001, () => {
-    console.log("server is listening")
-})
+server.listen(4000, () => {
+  console.log("server is listening");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import { io } from "socket.io-client";
+
+const socket = io.connect("http://localhost:4000");
+
 const App = () => {
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Hello" });
+    console.log(socket.connected);
+  };
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div>
+      <input type="text" placeholder="Message..." />
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
+  );
+};
+export default App;
